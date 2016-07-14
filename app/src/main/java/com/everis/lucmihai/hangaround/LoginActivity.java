f350590ea30e1f0310cc97f1236a8b08fbadf5fd
name0123 <mihai.lucian.lucut@est.fib.upc.edu>
2016-07-14 19:53:26 +0200

    login as guest and map

---
+++ app/src/main/java/com/everis/lucmihai/hangaround/LoginActivity.java
@@ -0,0 +1,32 @@
+package com.everis.lucmihai.hangaround;
+
+import android.app.Activity;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import java.sql.BatchUpdateException;
+
+import butterknife.BindView;
+import butterknife.OnClick;
+
+public class LoginActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+
+
+    }
+    @OnClick(R.id.bcontinue)
+    public void onClickContinue(View view){
+        Intent intent = new Intent(this, MapsActivity.class);
+        startActivity(intent);
+    }
+
+
+}
